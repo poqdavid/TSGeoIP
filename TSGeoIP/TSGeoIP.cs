273f8fe57ab522a762977a52137e8ca4506a75b7
poqdavid <poq-david@live.com>
2015-07-15 00:10:58 +0430

    Created the project

--- TSGeoIP/TSGeoIP.cs
+++ TSGeoIP/TSGeoIP.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Text;
 
 using Terraria;
+using TerrariaApi;
+using TerrariaApi.Server;
 using TShockAPI;
 using System.Threading;
-using TerrariaApi.Server;
+
 
 namespace TSGeoIP
 {
     [ApiVersion(1, 19)]
     public class TSGeoIP : TerrariaPlugin
     {
+        public override Version version
+        {
+            get { return new Version("1.0"); }
+        }
+ 
+        public override string Name
+        {
+            get { return "TSGeoIP Plugin"; }
+        }
+ 
+        public override string Author
+        {
+            get { return "POQDavid"; }
+        }
+
+        public TSGeoIP(Main game) : base(game)
+        {
+            Order -= 0;
+        }
+ 
+        public override void Initialize()
+        {
+
+        }
+ 
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose( disposing );
+        }
     }
 }

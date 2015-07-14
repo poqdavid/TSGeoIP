cf9bd70eb7d80783442a732ffff77497e38923e1
poqdavid <poq-david@live.com>
2015-07-14 23:28:03 +0430

    Created the project

---
+++ TSGeoIP/TSGeoIP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Terraria;
+using TShockAPI;
+using System.Threading;
+using TerrariaApi.Server;
+
+namespace TSGeoIP
+{
+    [ApiVersion(1, 19)]
+    public class TSGeoIP : TerrariaPlugin
+    {
+    }
+}
